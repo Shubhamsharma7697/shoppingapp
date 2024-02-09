e5b0013b425829fc0c03f673e2f489f1959f48c3
Dheeraj <dheerajyadav2507@gmail.com>
2024-02-09 18:33:08 +0530

    first

--- project/app/views.py
+++ project/app/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render,HttpResponse
 # Create your views 
 
 def home(request):
-    return HttpResponse("hello")
+    return HttpResponse("<h1>DheerAJ CHTUIYA HAO BADA BHAO</h1>")
